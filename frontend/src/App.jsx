a38531ed315526ea1ea6f63d2d3b847b7d565c0a
Gosha <lyhtyrageorgiu@gmail.com>
2024-04-19 22:54:02 +0300

    hotfix/docker_v23

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   useEffect(() => {
     async function fetchData() {
       try {
-        const response = await axios.get('http://hackathon-undefined.ru/hello/world:8000');
+        const response = await axios.get('http://hackathon-undefined.ru:8000/hello/world');
         setMessage(response.data.message);
       } catch (error) {
         console.error('Error fetching data:', error);
