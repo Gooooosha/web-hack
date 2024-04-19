ad1aa6f9751a9f8b0fef6fb49c28713bde49f800
Gosha <lyhtyrageorgiu@gmail.com>
2024-04-20 01:04:10 +0300

    hotfix/docker_v36

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,13 +10,13 @@ function App() {
   useEffect(() => {
     async function fetchData() {
       try {
-        const response = await fetch('http://hackathon-undefined.ru:8000');
+        const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
         if (!response.ok) {
           throw new Error('Network response was not ok');
         }
         const data = await response.json();
         console.log(data);
-        setMessage(data.message);
+        setMessage(data.title); // Предполагается, что в ответе есть поле "title"
       } catch (error) {
         console.error('Error fetching data:', error);
       }
