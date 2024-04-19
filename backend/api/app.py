40f0b810ed09e6c47452a9883134841177280fac
Gosha <lyhtyrageorgiu@gmail.com>
2024-04-20 00:02:30 +0300

    hotfix/docker_v33

--- backend/api/app.py
+++ backend/api/app.py
@@ -9,9 +9,9 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE"],  # Указываем разрешенные методы HTTP
-    allow_headers=["*"],
-    expose_headers=["*"]
+    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
+    allow_headers=["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
+                   "Authorization", "auth", "Project", "Stage", "Task", "Comment"],
 )
 
 
