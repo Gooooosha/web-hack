2ba85c5247cdef1b881cd4a40ad1f4acdf423e62
Gosha <lyhtyrageorgiu@gmail.com>
2024-04-20 01:31:27 +0300

    hotfix/docker_v38

--- backend/api/app.py
+++ backend/api/app.py
@@ -3,16 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-origins = ["*"]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
-    allow_headers=["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
-                   "Authorization", "auth", "Project", "Stage", "Task", "Comment"],
-)
 
 
 @app.get("/")
